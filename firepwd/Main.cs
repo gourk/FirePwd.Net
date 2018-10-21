eadf9c3a15632006a88715c468ba4cf240cf735e
Mafia1990 <attacker1369@yahoo.com>
2018-10-21 15:25:36 +0330

    --auto detect path --check login.json file and decrypt it

--- firepwd/Main.cs
+++ firepwd/Main.cs
@@ -6,6 +6,8 @@ using System.Text;
 using firepwd.Cryptography;
 using System.Text.RegularExpressions;
 using System.IO;
+using firepwd.net;
+using Newtonsoft.Json;
 
 namespace firepwd
 {
@@ -15,7 +17,8 @@ namespace firepwd
         {
             string MasterPwd = string.Empty;
             bool Verbose = false;
-            string filePath = string.Empty;
+           
+            string filePath = Directory.GetDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Mozilla\\Firefox\\Profiles"))[0];
 
             //Manage args
             for (int i = 0; i < args.Length; i++)
@@ -53,9 +56,9 @@ namespace firepwd
                 return 0;
             }
 
-            if (!File.Exists(Path.Combine(filePath,"signons.sqlite")))
+            if (!File.Exists(Path.Combine(filePath, "signons.sqlite")) && !File.Exists(Path.Combine(filePath, "logins.json")))
             {
-                Console.WriteLine("File key3.db not found.");
+                Console.WriteLine("File signons.sqlite/logins.json not found.");
                 return 0;
             }
 
@@ -147,32 +150,73 @@ namespace firepwd
                 Console.WriteLine("Private key = " + privateKey.ToString());
             }
             // decrypt username and password
-            using (SQLiteConnection cnn = new SQLiteConnection("Data Source="+Path.Combine(filePath,"signons.sqlite")))
-            {
-                cnn.Open();
-                SQLiteCommand mycommand = new SQLiteCommand(cnn);
-                mycommand.CommandText = "select hostname, encryptedUsername, encryptedPassword, guid, encType from moz_logins;";
-                SQLiteDataReader reader = mycommand.ExecuteReader();
-                dt.Load(reader);
-            }
 
-            foreach (DataRow row in dt.Rows)
+            if (File.Exists(Path.Combine(filePath, "signons.sqlite")))
             {
-                Asn1DerObject user = asn.Parse(Convert.FromBase64String(row["encryptedUsername"].ToString()));
-                Asn1DerObject pwd = asn.Parse(Convert.FromBase64String(row["encryptedPassword"].ToString()));
-                if (Verbose)
+
+                using (SQLiteConnection cnn = new SQLiteConnection("Data Source=" + Path.Combine(filePath, "signons.sqlite")))
                 {
-                    Console.WriteLine("User= " + user.ToString());
-                    Console.WriteLine("Pwd= " + pwd.ToString());
+                    cnn.Open();
+                    SQLiteCommand mycommand = new SQLiteCommand(cnn);
+                    mycommand.CommandText = "select hostname, encryptedUsername, encryptedPassword, guid, encType from moz_logins;";
+                    SQLiteDataReader reader = mycommand.ExecuteReader();
+                    dt.Load(reader);
                 }
 
-                string hostname = row["hostname"].ToString();
-                string decryptedUser = TripleDESHelper.DESCBCDecryptor(privateKey, user.objects[0].objects[1].objects[1].Data, user.objects[0].objects[2].Data);
-                string decryptedPwd = TripleDESHelper.DESCBCDecryptor(privateKey, pwd.objects[0].objects[1].objects[1].Data, pwd.objects[0].objects[2].Data);
+                foreach (DataRow row in dt.Rows)
+                {
+                    Asn1DerObject user = asn.Parse(Convert.FromBase64String(row["encryptedUsername"].ToString()));
+                    Asn1DerObject pwd = asn.Parse(Convert.FromBase64String(row["encryptedPassword"].ToString()));
+                    if (Verbose)
+                    {
+                        Console.WriteLine("User= " + user.ToString());
+                        Console.WriteLine("Pwd= " + pwd.ToString());
+                    }
 
-                Console.WriteLine(hostname + " " + Regex.Replace(decryptedUser, @"[^\u0020-\u007F]", "") + " " + Regex.Replace(decryptedPwd, @"[^\u0020-\u007F]", ""));
+                    string hostname = row["hostname"].ToString();
+                    string decryptedUser = TripleDESHelper.DESCBCDecryptor(privateKey, user.objects[0].objects[1].objects[1].Data, user.objects[0].objects[2].Data);
+                    string decryptedPwd = TripleDESHelper.DESCBCDecryptor(privateKey, pwd.objects[0].objects[1].objects[1].Data, pwd.objects[0].objects[2].Data);
+
+                    Console.WriteLine(hostname + " " + Regex.Replace(decryptedUser, @"[^\u0020-\u007F]", "") + " " + Regex.Replace(decryptedPwd, @"[^\u0020-\u007F]", ""));
+                }
             }
+            else if (File.Exists(Path.Combine(filePath, "logins.json")))
+            {
+                FFLogins ffLoginData;
+                using (StreamReader sr = new StreamReader(Path.Combine(filePath, "logins.json")))
+                {
+                    string json = sr.ReadToEnd();
+                    ffLoginData = JsonConvert.DeserializeObject<FFLogins>(json);
+                }
 
+                foreach (LoginData loginData in ffLoginData.logins)
+                {
+                    try
+                    {
+                        Asn1DerObject user = asn.Parse(Convert.FromBase64String(loginData.encryptedUsername));
+                        Asn1DerObject pwd = asn.Parse(Convert.FromBase64String(loginData.encryptedPassword));
+                        if (Verbose)
+                        {
+                            Console.WriteLine("User= " + user.ToString());
+                            Console.WriteLine("Pwd= " + pwd.ToString());
+                        }
+
+                        string hostname = loginData.hostname;
+                        string decryptedUser = TripleDESHelper.DESCBCDecryptor(privateKey, user.objects[0].objects[1].objects[1].Data, user.objects[0].objects[2].Data);
+                        string decryptedPwd = TripleDESHelper.DESCBCDecryptor(privateKey, pwd.objects[0].objects[1].objects[1].Data, pwd.objects[0].objects[2].Data);
+
+                        Console.WriteLine(hostname + " " + Regex.Replace(decryptedUser, @"[^\u0020-\u007F]", "") + " " + Regex.Replace(decryptedPwd, @"[^\u0020-\u007F]", ""));
+               
+                    }
+                    catch (Exception e)
+                    {
+                        continue;
+                    }
+                }
+            
+            
+            }
+            Console.Read();
             return 0;
         }
     }
